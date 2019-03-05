8108346b7c18448dacdbae74c14ad7957b081df4
Fenix <fbamperius@gmail.com>
2019-03-05 15:03:45 +0100

    Agregar archivos de proyecto.

---
+++ sharing_sense/sharing_sense.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		sharing_sense.ino
+ Created:	05/03/2019 15:03:39
+ Author:	jesus
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
